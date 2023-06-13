2a66e1862fc78e9e4336a4c182f8dc502b0f9472
dbodonf01 <131864556+dbodonf01@users.noreply.github.com>
2023-06-13 09:39:24 +0200

    Update presentacion.java

--- presentacion.java
+++ presentacion.java
@@ -1,18 +1,8 @@
-bodon
-public class presentacion {
-
-	public static void main(String[] args) {		
-		System.out.println("Hola Mundo, mi nombre es David Bodon");
-	}
-
-}
-
-
-
 public class presentacion
 {
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Hector!");
+		System.out.println("Hola Mundo, mi nombre es David Bodon");
 	}
 }
 main
