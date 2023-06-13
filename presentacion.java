0e1d79f2a222e6e9cae2f00a8fd2a20cef3a46a2
Álvaro <132890243+geraaldess01@users.noreply.github.com>
2023-06-13 09:29:32 +0200

    Update presentacion.java

--- presentacion.java
+++ presentacion.java
@@ -4,6 +4,6 @@
 public class presentacion
 {
 	public static void main(String[] args) {
-		System.out.println("Hola mi nombre es xxxx!");
+		System.out.println("Hola mi nombre es Álvaro Geraldes Simón!");
 	}
 }
